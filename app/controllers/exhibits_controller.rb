6ab71775e85aa0f62c87d199e9dbfd9544f30712
leo <leo.kalshteyn@gmail.com>
2019-08-20 20:59:23 -0700

    Cat/Exhibit controller edit

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,2 +1,26 @@
 class ExhibitsController < ApplicationController
+
+  def new
+    @exhibit = Exhibit.new
+  end
+
+  def create
+    @exhibit = Exhibit.new(exhibit_params)
+    if @exhibit.save
+      redirect_to exhibits_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @exhibits = exhibit.all
+  end
+
+  private
+
+  def exhibit_params
+    params.require(:exhibit).permit(:name)
+  end
+
 end
