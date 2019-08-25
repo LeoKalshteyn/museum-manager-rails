de032fceaa983727f6c65105c4bb195fe5b5b7a9
leo <leo.kalshteyn@gmail.com>
2019-08-24 20:45:15 -0700

    added create, edit and delete for exhibits

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -6,9 +6,6 @@ class ExhibitsController < ApplicationController
   end
 
   def create
-    @exhibit = Exhibit.new(exhibit_params)
-    if @exhibit.save
-      redirect_to exhibits_path
     if current_user
       @user = current_user
       @exhibit = @user.exhibits.build(exhibit_params)
@@ -16,24 +13,42 @@ class ExhibitsController < ApplicationController
           redirect_to @exhibit
         end
     else
-      render :new
       redirect :new
     end
   end
 
   def index
-    @exhibits = exhibit.all
     if params[:user_id]
       @exhibits = User.find(params[:user_id]).exhibits
     elsif params[:category_id]
         @exhibits = Category.find(params[:category_id]).exhibits
     else
-      @exhibits = Exhibit.all
+      @exhibits = Category.all
     end
   end
 
   def show
     @exhibits = Exhibit.find_by_id(params[:id])
+  end
+
+  def edit
+  selected_exhibit
+  @category = Category.find_by(id: params[:category_id])
+    if @exhibit.user_id != current_user.id
+      redirect_to categories_path
+    end
+  end
+
+  def update
+    exhibit = Exhibit.find(params[:id])
+    exhibit.update(exhibit_params)
+    redirect_to exhibit
+  end
+
+  def destroy
+    selected_exhibit
+    @exhibit.destroy
+    redirect_to exhibits_path
   end
 
   private
@@ -42,10 +57,11 @@ class ExhibitsController < ApplicationController
     params.require(:exhibit).permit(:user_id, :category_id, :name, :origin, :age)
   end
 
-  def set_exhibit
+  def selected_exhibit
     @exhibit = Exhibit.find_by_id(params[:id])
     if !@exhibit
       redirect_to exhibits_path
     end
   end
+
 end
