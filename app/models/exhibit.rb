def723ec994c0737f2a3f6815626578eb8d24c84
leo <leo.kalshteyn@gmail.com>
2019-09-03 23:17:21 -0700

    edited layout of pages

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -3,6 +3,10 @@ class Exhibit < ApplicationRecord
   belongs_to :museum
   belongs_to :transit_status
 
+  validates :open_date, date: true
+  validates :close_date, date: true
+
+
   scope :most_exhibits, -> {
     select('exhibits.*, COUNT(exhibits.museum_id) AS museums_count')
     .group('museum_id')
